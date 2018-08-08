d7e350778efa8c8f446b29ea913e27378ce3c491
DESKTOP-BQTGEL0\Ferdinand <xiong@in.tum.de>
2018-08-08 22:26:29 +0200

    initial commit

---
+++ app/src/main/java/com/example/ferdinand/myfirstandroidapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.ferdinand.myfirstandroidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //Called when user taps the Send button
+    public void sendMessage(View view){
+        //Do Something in response to button
+    }
+}
